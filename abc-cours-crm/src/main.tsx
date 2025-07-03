4bf383d02ac7cde1ed2e543fe8eb3842a302f61e
Florian Cherubin <floriancherubin@gmail.com>
2025-07-03 20:54:37 +0200

    Setup routing : route / vers page Home

--- abc-cours-crm/src/main.tsx
+++ abc-cours-crm/src/main.tsx
@@ -2,13 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Login } from "./pages";
+import Home from "./pages/Home";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
