dd6c13924d7d4845724a42ee90dc880e114e4a79
Florian Cherubin <floriancherubin@gmail.com>
2025-07-03 21:25:18 +0200

    Refactor : Home page renommée en Admin page

--- abc-cours-crm/src/main.tsx
+++ abc-cours-crm/src/main.tsx
@@ -2,13 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
+import Admin from "./pages/Admin";
+import Login from "./pages/Login";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/admin" element={<Admin />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
