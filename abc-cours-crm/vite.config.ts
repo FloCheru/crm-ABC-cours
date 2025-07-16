71f82532ba696e018d3e4a85f6e7536c1ecd0a75
Florian Cherubin <floriancherubin@gmail.com>
2025-07-16 22:25:32 +0200

    fix: use simplified GitHub Pages workflow

--- abc-cours-crm/vite.config.ts
+++ abc-cours-crm/vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/abc-cours-crm/", // Pour GitHub Pages
+  base: "/crm-ABC-cours/", // Pour GitHub Pages
   build: {
     outDir: "dist",
     assetsDir: "assets",
